462e8bc295709e71e2d3565c21589e5954ba6d18
Avinash Singh <rajputavinash124@gmail.com>
2022-10-16 20:18:27 +0530

    chnages

--- Hello/settings.py
+++ Hello/settings.py
@@ -151,11 +151,11 @@ USE_TZ = True
 DISABLE_COLLECTSTATIC=1
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 STATIC_URL = '/static/'
-
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static"),]
+if DEBUG:
+    STATICFILES_DIRS = [os.path.join(BASE_DIR, "static"),]
 # PROJECT_ROOT   =   os.path.join(os.path.abspath(__file__))
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+else:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
