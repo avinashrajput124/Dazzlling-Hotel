bc3a530bc71dcef0cd4e36dd5b917410285dc7f2
Avinash Singh <rajputavinash124@gmail.com>
2022-10-16 22:57:50 +0530

    only write static in staticroot

--- Hello/settings.py
+++ Hello/settings.py
@@ -26,7 +26,7 @@ MESSAGE_TAGS = {
  }
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = (Path(__file__).resolve().parent.parent)
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 # str add in base dir avinash
 
@@ -151,15 +151,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 DISABLE_COLLECTSTATIC=1
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static"),]
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+    ]
 # PROJECT_ROOT   =   os.path.join(os.path.abspath(__file__))
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = '/static/'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
