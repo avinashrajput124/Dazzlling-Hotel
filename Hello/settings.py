ac46c4c42b4f67c08b153d33e41329e0df1b541a
Avinash Singh <rajputavinash124@gmail.com>
2022-10-16 19:39:05 +0530

    Added database or staticroot

--- Hello/settings.py
+++ Hello/settings.py
@@ -93,12 +93,24 @@ WSGI_APPLICATION = 'Hello.wsgi.application'
 import dj_database_url 
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+ 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'dazzlling',
+        'USER': 'dazzlling',
+        'PASSWORD': 'Vpsroot@123',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
+
 
 prod_db  =  dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(prod_db)
@@ -145,7 +157,7 @@ if DEBUG:
     ]
 # PROJECT_ROOT   =   os.path.join(os.path.abspath(__file__))
 else:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
