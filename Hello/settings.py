3f901798508827f88685daa9724690acd4b0a8ff
Avinash Singh <rajputavinash124@gmail.com>
2022-11-11 12:42:31 +0530

    add allowed host

--- Hello/settings.py
+++ Hello/settings.py
@@ -40,7 +40,7 @@ SECRET_KEY = 'django-insecure-p@2r&c^)lu=1#89^=0w&-%nyd+4#@jk+vm2ms+o^d+%-i(h!@8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['daazzllingapp.herokuapp.com']
 
 AUTH_USER_MODEL = 'ello.User'
 # Application definition
